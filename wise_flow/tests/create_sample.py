15fb8161febffd8ed7a3e968ee9d12e422d3283d
feelfine1977 <feelfine1977@yahoo.de>
2024-03-14 17:48:05 +0100

    end of day comit

--- wise_flow/tests/create_sample.py
+++ wise_flow/tests/create_sample.py
@@ -4,7 +4,7 @@ from pm4py.algo.filtering.log.variants import variants_filter
 
 
 
-def create_sample(df, num_cases=100, sample_csv_path=None):
+def create_sample_log(df, num_cases=100, sample_csv_path=None, xes_path=None):
     """
     Create a sample of the original dataset
     """
@@ -12,12 +12,20 @@ def create_sample(df, num_cases=100, sample_csv_path=None):
         sample_csv_path = 'C:\Code\Github\WISE\wise_flow\\tests\data/BPI_Challenge_2019_sample.csv'
     # Load the dataset
     dataframe = pm4py.format_dataframe(df, case_id='case_concept_name', activity_key='event_concept_name', timestamp_key='event_time_timestamp')
-    sampled_df = pm4py.sample_cases(dataframe, num_cases, case_id_key='case:concept:name')
-    sampled_df.to_csv(sample_csv_path, index=False)
+    sampled_log = pm4py.sample_cases(dataframe, num_cases, case_id_key='case:concept:name')
+    if xes_path is not None:
+        pm4py.write_xes(sampled_log, xes_path)
+        print(f"Sample XES file created at: {xes_path}")
+    if sample_csv_path is not None:
+        sampled_log.to_csv(sample_csv_path, index=False)
 
     print(f"Sample CSV file created at: {sample_csv_path}")
 
-    return sampled_df
+    return sampled_log
 
-dataframe = pd.read_csv('C:\Code\Github\WISE\wise_flow\data\BPI_Challenge_2019.csv', sep=',')
-df = create_sample(dataframe, 1000)
+
+if __name__ == "__main__":
+    csv_path = "C:\Code\Github\WISE\wise_flow\\tests\data/BPI_Challenge_2019_sample.csv"
+    xes_path = "C:\Code\Github\WISE\wise_flow\\tests\data/BPI_Challenge_2019_sample.xes"
+    dataframe = pd.read_csv('C:\Code\Github\WISE\wise_flow\data\BPI_Challenge_2019.csv', sep=',')
+    log = create_sample_log(dataframe, 1000, csv_path, xes_path)
